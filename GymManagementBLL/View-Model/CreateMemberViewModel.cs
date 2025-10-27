d06045710bf7ab4568acc405f28810623ddbff3b
1Olamamdouh <olamamdouh022@gmail.com>
2025-10-27 05:28:50 +0300

    MemberServicces [Part01]

---
+++ GymManagementBLL/View-Model/CreateMemberViewModel.cs
@@ -0,0 +1,61 @@
+﻿using GymManagementDAL.Entity.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.View_Model
+{
+    internal class CreateMemberViewModel 
+    {
+        [Required(ErrorMessage = " Name is Required")]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 50 ")]
+        [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "Name must contain letters or spaces only ")]
+        public string Name { get; set; } = null!;
+        
+        
+        [Required(ErrorMessage ="Email is Required")]
+        [EmailAddress (ErrorMessage ="Invalid Email format")] //App to validate email format
+        [DataType(DataType.EmailAddress)] //UI Hint for email input
+        [StringLength(100, MinimumLength = 5, ErrorMessage ="Email must be between 5 and 100")]
+        public string Email { get; set; } = null!;
+
+        [Required(ErrorMessage = "Phone is Required")]
+        [Phone (ErrorMessage = " Invalid phone formate")]
+        [RegularExpression(@"^(010|011|012|015)\d{8}$")]
+        public string PhoneNumber { get; set; } = null!;
+
+        [Required(ErrorMessage ="DataOfBirth is Required")]
+        [DataType(DataType.Date)]
+        public DateOnly DateOfBirth { get; set; }
+
+        [Required(ErrorMessage ="Building Number is Required")]
+        [Range(1,9000, ErrorMessage ="Building Number between 1 and 9000")]
+        public int BuildingNumber { get; set; }
+
+        [Required(ErrorMessage = "City is Required")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Email must be between 2 and 30")]
+        public string City { get; set; }= null!;
+
+
+        [Required(ErrorMessage = "Street is Required")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Email must be between 2 and 30")]
+        public string street { get; set; } = null!;
+
+
+        [Required(ErrorMessage = "Gender is Required")]
+        public Gender Gender { get; set; }
+
+
+        [Required (ErrorMessage = "Health Reacord is required ")]
+        public HealthRecordViewModel HealthRecord { get; set; } = null!; // Navigation property to HealthRecordViewModel
+
+
+
+
+    }
+
+}
---
+++ GymManagementBLL/View-Model/HealthRecordViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GymManagementBLL.View_Model
+{
+    internal class HealthRecordViewModel
+    {
+
+        //The health record of a gym member includes the following properties:
+
+        [Required(ErrorMessage = "Height is required")]
+        [Range(0.1, 300, ErrorMessage = "Height must be between 0.1 cm and 300 cm")]
+        public decimal Height { get; set; } // in centimeters
+
+        [Required(ErrorMessage = "Weight is required")]
+        [Range(1, 350, ErrorMessage = "Weight must be between 1 kg and 350 kg")]
+        public decimal Weight { get; set; } // in kilograms
+
+        [Required(ErrorMessage = "Bool Type Is Required")]
+        [StringLength(3, ErrorMessage = "Blood Type is max 3")]
+        public string Booltype { get; set; } = null!; // e.g., A+, O-, etc.
+        public string? Notes { get; set; } // Optional additional notes
+
+    }
+}
